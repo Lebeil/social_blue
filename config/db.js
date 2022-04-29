2decd86e5a6e44750798ea528d87d58c677c23b8
Liedel <liedel75@gmail.com>
2022-04-29 17:25:50 +0200

    fix db MongoDB

--- config/db.js
+++ config/db.js
@@ -23,7 +23,7 @@ const mongoose = require("mongoose");
 
 mongoose
     .connect(
-        `mongodb+srv://${process.env.DB_USER_PASS}@cluster0.k5wpj.mongodb.net/test?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true`
+        `mongodb+srv://${process.env.DB_USER_PASS}@cluster0.k5wpj.mongodb.net/social_blue?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true`
     )
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log("Failed to connect to MongoDB", err));
