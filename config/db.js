4edda5e8d62986a1329c144479741dadec4b8215
Liedel <liedel75@gmail.com>
2022-04-29 16:22:39 +0200

    add models, controllers & fix mongoose connect

---
+++ config/db.js
@@ -0,0 +1,29 @@
+/*const { MongoClient } = require("mongodb");
+
+// Replace the following with your Atlas connection string
+const url = `mongodb+srv://${process.env.DB_USER_PASS}@cluster0.k5wpj.mongodb.net/test?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true`;
+const client = new MongoClient(url);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);*/
+
+const mongoose = require("mongoose");
+
+mongoose
+    .connect(
+        `mongodb+srv://${process.env.DB_USER_PASS}@cluster0.k5wpj.mongodb.net/test?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true`
+    )
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log("Failed to connect to MongoDB", err));
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const userRoutes = require('./routes/user.routes')
+const bodyParser = require('body-parser')
 require('dotenv').config({path: './config/.env'})
+require('./config/db')
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//routes
+app.use('/api/user', userRoutes)
 
 app.listen(process.env.PORT, ()=> {
     console.log(`Listening on port ${process.env.PORT}`);
