d97767390ba29c85b825a624e385e844204542e2
Liedel <liedel75@gmail.com>
2022-05-13 16:20:31 +0200

    add like-post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -59,3 +59,43 @@ module.exports.deletePost = (req, res)=> {
         }
     )
 }
+
+module.exports.likePost = async (req, res)=> {
+    if(!ObjectID.isValid(req.params.id))
+        return res.status(400).send("ID unknow : " +  req.params.id);
+
+    try {
+        // tableau de like pour l'article
+        await PostModel.findByIdAndUpdate(
+            req.params.id,
+            {
+                $addToSet: { likers: req.body.id }
+            },
+            { new: true},
+            (err, docs)=> {
+                if(err) return res.status(400).send(err);
+                else return res.send(docs)
+            }
+        );
+        // tableau de like pour l'utilisateur
+        await UserModel.findByIdAndUpdate(
+            req.body.id,
+            {
+                $addToSet: { likes: req.params.id}
+            },
+            {
+                new: true
+            },
+            (err, docs)=> {
+                if(!err) return res.status(200).send(docs);
+                else return res.status(400).send(err);
+            }
+        )
+    } catch(err) {
+        return res.status(400).send(err);
+    }
+}
+
+module.exports.unlikePost = async (req, res)=> {
+
+}
