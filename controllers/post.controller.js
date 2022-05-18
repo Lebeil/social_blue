f6232cdea3c53213311bfb3e2dc12c2d4da94498
Liedel <liedel75@gmail.com>
2022-05-18 21:18:20 +0200

    add photo post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,6 +1,10 @@
 const PostModel = require('../models/post.model');
 const UserModel = require('../models/user.model');
+const {uploadErrors} = require("../utils/errors.utils");
 const ObjectID = require('mongoose').Types.ObjectId;
+const fs = require('fs');
+const {promisify} = require('util');
+const pipeline = promisify(require('stream').pipeline);
 
 module.exports.readPost = (req, res) => {
     PostModel.find((err, docs) => {
@@ -10,21 +14,49 @@ module.exports.readPost = (req, res) => {
 }
 
 module.exports.createPost = async (req, res) => {
-    const newPost = new PostModel({
-        posterId: req.body.posterId,
-        message: req.body.message,
-        video: req.body.video,
-        likers: [],
-        comments: [],
-    });
+    let fileName;
 
-    try {
-        const post = await newPost.save();
-        return res.status(201).json(post);
-    } catch (err) {
-        return res.status(400).send(err)
+    if(req.file !== null) {
+        try {
+            if (
+                req.file.detectedMimeType !== "image/jpg" &&
+                req.file.detectedMimeType !== "image/png" &&
+                req.file.detectedMimeType !== "image/jpeg"
+            )
+                throw Error("invalid file");
+
+            if (req.file.size > 500000) throw Error("max size")
+        } catch (err) {
+            const errors = uploadErrors(err)
+            return res.status(201).json(errors);
+        }
+
+        fileName = req.body.posterId + Date.now() + ".jpg";
+
+        await pipeline(
+            req.file.stream,
+            fs.createWriteStream(
+                `${__dirname}/../client/public/uploads/posts/${fileName}`
+            )
+        )
+
+        const newPost = new PostModel({
+            posterId: req.body.posterId,
+            message: req.body.message,
+            picture: req.file !== null ? "./uploads/posts/" + fileName : "",
+            video: req.body.video,
+            likers: [],
+            comments: [],
+        });
+
+        try {
+            const post = await newPost.save();
+            return res.status(201).json(post);
+        } catch (err) {
+            return res.status(400).send(err)
+        }
+
     }
-
 }
 
 module.exports.updatePost = (req, res) => {
