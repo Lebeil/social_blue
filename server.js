87136678dad96488a4db5b1412fcc07e6933b5fc
Liedel <liedel75@gmail.com>
2022-05-19 12:30:25 +0200

    add Cors

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ require('dotenv').config({path: './config/.env'})
 require('./config/db')
 const { checkUser, requireAuth } = require('./middleware/auth.middleware');
 const app = express();
+const cors = require('cors');
+
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'methods': 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    'preflightContinue': false
+}
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
