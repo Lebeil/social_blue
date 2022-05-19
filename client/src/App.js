9a914b44f83897c7263249ee8fd3d9ab7eeb6f5c
Liedel <liedel75@gmail.com>
2022-05-19 16:05:21 +0200

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Routes from './components/Routes';
 
 const App = () => {
   return (
       <div>
-        <h1>bonjour</h1>
+        <Routes/>
       </div>
   );
 };
--- client/src/components/Routes/index.js
+++ client/src/components/Routes/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
+import Home from "../../pages/Home";
+import Profil from "../../pages/Profil";
+import Trending from "../../pages/Trending";
 
 const Index = () => {
     return (
-        <div>
-
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/profil" element={<Profil/>}/>
+                <Route path="/trending" element={<Trending/>}/>
+                <Route path="*" element={<Navigate to="/" />}/>
+            </Routes>
+        </Router>
     );
 };
 
