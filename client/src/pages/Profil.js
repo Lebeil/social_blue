fa97529ed348d763c60ed80e796ae50b49de957e
Liedel <liedel75@gmail.com>
2022-05-21 15:00:21 +0200

    add login

--- client/src/pages/Profil.js
+++ client/src/pages/Profil.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import Log from "../components/Log";
 
 const Profil = () => {
+
     return (
-        <div>
-            Profil
+        <div className="profil-page">
+            <div className="log-container">
+                <Log signin={false} signup={true}/>
+                <div className="img-container">
+                    <img src="/img/log.svg" alt="img-log"/>
+                </div>
+            </div>
         </div>
     );
 };
