39712e1cd5f105f5f2e4e52d5a316a11314b9fcf
Liedel <liedel75@gmail.com>
2022-05-30 12:53:44 +0200

    add useContext uid

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Routes from './components/Routes';
+import {UidContext} from "./components/AppContext";
+import axios from "axios";
 
 const App = () => {
-  return (
-      <div>
-        <Routes/>
-      </div>
-  );
+    const [uid, setUid] = useState(null);
+
+    useEffect(() => {
+        const fetchToken = async()=> {
+            axios({
+                method: 'get',
+                url: `${process.env.REACT_APP_API_URL}jwtid`,
+                withCredentials: true,
+            })
+                .then((res) =>
+                    setUid(res.data)
+                )
+                .catch((err) => console.log("No Token"));
+        };
+        fetchToken();
+    }, [uid]);
+
+    return (
+        <UidContext.Provider value={uid}>
+            <Routes/>
+        </UidContext.Provider>
+    );
 };
 
 export default App;
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: process.env.CLIENT_URL,
     credentials: true,
     'allowedHeaders': ['sessionId', 'Content-Type'],
     'exposedHeaders': ['sessionId'],
