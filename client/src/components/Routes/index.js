e86a22d6acc9678db8b032be0dd0cbcba0e8f9ac
Liedel <liedel75@gmail.com>
2022-05-30 16:10:08 +0200

    add navbar & remove cookie

--- client/src/components/Routes/index.js
+++ client/src/components/Routes/index.js
@@ -3,10 +3,12 @@ import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom
 import Home from "../../pages/Home";
 import Profil from "../../pages/Profil";
 import Trending from "../../pages/Trending";
+import Navbar from "../Navbar";
 
 const Index = () => {
     return (
         <Router>
+            <Navbar/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/profil" element={<Profil/>}/>
